14844115f938ca107f284c417db830ca0f635e07
Jesse Rushlow <jr@geeshoe.com>
2020-05-19 08:40:01 -0400

    created manipulator docker compose files

---
+++ src/Docker/ComposeFileManipulator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Symfony\Bundle\MakerBundle\Docker;
+
+use Symfony\Bundle\MakerBundle\Util\YamlSourceManipulator;
+
+/**
+ * @TODO Move to Util namespace
+ */
+class ComposeFileManipulator
+{
+    //@TODO hmm, probably need to come up with a current ver and supported ver array
+    public const COMPOSE_FILE_VERSION = '3.7';
+
+    private $contents;
+    private $composeData;
+
+    public function __construct(string $contents = '')
+    {
+        $this->contents = $contents;
+
+        if ($this->contents === '') {
+            $this->composeData = ComposeFile::getBasicStructure(self::COMPOSE_FILE_VERSION);
+        } else {
+            $this->composeData = (new YamlSourceManipulator($this->contents))->getData();
+        }
+    }
+
+    public function getData(): array
+    {
+        return $this->composeData;
+    }
+
+    public function addDockerService(string $name, array $details): void
+    {
+        $this->composeData['services'][$name] = $details;
+    }
+
+    public function removeDockerService(string $name): void
+    {
+        unset($this->composeData['services'][$name]);
+    }
+}
---
+++ tests/Docker/ComposeFileManipulatorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Symfony\Bundle\MakerBundle\Tests\Docker;
+
+use Symfony\Bundle\MakerBundle\Docker\ComposeFileManipulator;
+use PHPUnit\Framework\TestCase;
+
+class ComposeFileManipulatorTest extends TestCase
+{
+    public function testCurrentVersion(): void
+    {
+        self::assertSame('3.7', ComposeFileManipulator::COMPOSE_FILE_VERSION);
+    }
+
+    public function testBasicComposeFileReturnedWhenContentsIsEmpty(): void
+    {
+        $expected = [
+            'version' => '3.7',
+            'services' => []
+        ];
+
+        self::assertSame($expected, (new ComposeFileManipulator())->getData());
+    }
+
+    public function testContentsParsedWhenContentStringNotEmpty(): void
+    {
+        $contentString = "version: '3.7'\nservices: {}\nnetworks:\n App: Bridge\n";
+
+        $expected = [
+            'version' => '3.7',
+            'services' => [],
+            'networks' => [
+                'App' => 'Bridge'
+            ]
+        ];
+
+        self::assertSame($expected, (new ComposeFileManipulator($contentString))->getData());
+    }
+
+    public function testAddDockerService(): void
+    {
+        $expectedServiceProperties = [
+            'image' => 'mariadb:latest',
+            'port' => 1234,
+            'networks' => ['front', 'back']
+        ];
+
+        $manipulator = new ComposeFileManipulator();
+        $manipulator->addDockerService('database', $expectedServiceProperties);
+
+        $results = $manipulator->getData();
+
+        self::assertArrayHasKey('database', $results['services']);
+        self::assertSame($results['services']['database'], $expectedServiceProperties);
+    }
+
+    public function testRemoveDockerService(): void
+    {
+        $manipulator = new ComposeFileManipulator(
+            "version: '3.7'\nservices: { database: }\n"
+        );
+
+        $manipulator->removeDockerService('database');
+        $result = $manipulator->getData();
+
+        self::assertArrayNotHasKey('database', $result['services']);
+    }
+}
