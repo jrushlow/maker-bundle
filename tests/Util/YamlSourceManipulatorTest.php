7f6cb8d93e52b5b1b5e288a26e63fb19abe70c6b
Jesse Rushlow <jr@geeshoe.com>
2020-05-22 17:07:28 -0400

    fix for index associative arrays
    Thanks to @weaverryan for th fix

--- tests/Util/YamlSourceManipulatorTest.php
+++ tests/Util/YamlSourceManipulatorTest.php
@@ -48,16 +48,17 @@ class YamlSourceManipulatorTest extends TestCase
         $this->assertSame($expectedSource, $actualContents);
     }
 
+    //@TODO Replace with the ysm preferred test format
     public function testCommentAddedToRoot(): void
     {
         $manipulator = new YamlSourceManipulator("key: value\n");
 
         $data = $manipulator->getData();
-        $data[] = $manipulator->createCommentLine('I am a comment.');
+        $data[] = $manipulator->createCommentLine(' I am a comment.');
 
         $manipulator->setData($data);
 
-        self::assertSame("key: value\n# I am a comment.");
+        self::assertSame("key: value\n# I am a comment.\n", $manipulator->getContents());
     }
 
     private function getYamlDataTests()
