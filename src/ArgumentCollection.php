a99b9d89fa798ec7ea31b6d8013f5b254be10269
Jesse Rushlow <jr@geeshoe.com>
2020-05-31 00:25:03 -0400

    does the arg exist in the collection

--- src/ArgumentCollection.php
+++ src/ArgumentCollection.php
@@ -13,7 +13,16 @@ class ArgumentCollection implements \IteratorAggregate
 
     public function getArgument(string $name): MakerArgument
     {
+        $this->argumentExists($name);
+
         return $this->arguments[$name];
+    }
+
+    public function replaceArgument(MakerArgument $argument): void
+    {
+        $this->argumentExists($argument->getName());
+
+        $this->arguments[$argument->getName()] = $argument;
     }
 
     public function removeArgument(string $name): void
@@ -23,16 +32,27 @@ class ArgumentCollection implements \IteratorAggregate
 
     public function setArgumentValue(string $name, $value): void
     {
+        $this->argumentExists($name);
+
         $this->arguments[$name]->setValue($value);
     }
 
     public function getArgumentValue(string $name)
     {
+        $this->argumentExists($name);
+
         return $this->arguments[$name]->getValue();
     }
 
     public function getIterator()
     {
         return new \ArrayIterator($this->arguments);
+    }
+
+    private function argumentExists(string $name): void
+    {
+        if (!isset($this->arguments[$name])) {
+            // @TODO Throw arg doesnt exist exception
+        }
     }
 }
